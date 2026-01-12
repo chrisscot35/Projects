e81765fdefc511a53869c9378d737d3a02102484
chrisscot35 <chrisscot12@yahoo.com>
2026-01-12 14:26:04 -0600

    Updated file

--- variables.tf
+++ variables.tf
@@ -22,24 +22,24 @@ variable "availability_zones" {
 variable "public_subnet_cidrs" {
   type        = list(string)
   default = [
-    "10.0.2.0/24",
-    "10.0.0.0/24"
+    "10.0.1.0/24",
+    "10.0.2.0/24"
   ]
 }
 
 variable "app_subnet_cidrs" {
   type        = list(string)
   default = [
-    "10.0.1.0/24",
-    "10.0.10.0/24"
+    "10.0.3.0/24",
+    "10.0.4.0/24"
   ]
 }
 
 variable "db_subnet_cidrs" {
   type        = list(string)
   default = [
-    "10.0.20.0/24",
-    "10.0.30.0/24"
+    "10.0.5.0/24",
+    "10.0.6.0/24"
   ]
 }
 
--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
@@ -14,20 +14,20 @@ data "aws_availability_zones" "az1" {}
 
 resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.1.0/24"
   availability_zone       = var.availability_zones[0]
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "app_a" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.3.0/24"
   availability_zone = var.availability_zones[0]
 }
 
 resource "aws_subnet" "db_a" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.20.0/24"
+  cidr_block        = "10.0.5.0/24"
   availability_zone = var.availability_zones[0]
 }
 
@@ -35,20 +35,20 @@ data "aws_availability_zones" "az2" {}
 
 resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.0.0/24"
+  cidr_block              = "10.0.2.0/24"
   availability_zone       = var.availability_zones[1]
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "app_b" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.10.0/24"
+  cidr_block        = "10.0.4.0/24"
   availability_zone = data.aws_availability_zones.az2.names[1]
 }
 
 resource "aws_subnet" "db_b" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.30.0/24"
+  cidr_block        = "10.0.6.0/24"
   availability_zone = data.aws_availability_zones.az2.names[1]
 }
 
